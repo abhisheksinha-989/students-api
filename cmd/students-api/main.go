2e14a3b1d2ace5844b1ffaff94f17db91c7020a4
abhisheksinha-989 <amansinha.989@gmail.com>
2026-08-29 11:10:09 +0530

    request to validation

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/abhisheksinha-989/students-api/internal/config"
+	"github.com/abhisheksinha-989/students-api/internal/http/handlers/student"
 )
 
 func main() {
@@ -18,12 +19,9 @@ func main() {
 	cfg := config.MustLoad()
 
 	// database setup
-	
 	// setup router
 	router := http.NewServeMux()
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to students api"))
-	})
+	router.HandleFunc("POST /api/students", student.New())
 
 	// setup server
 	server := http.Server{
