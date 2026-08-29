ea3ec218a5e70965739cc352d453c68915d3a199
abhisheksinha-989 <amansinha.989@gmail.com>
2026-08-29 14:52:08 +0530

    get student

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,5 +1,7 @@
 package storage
+import "github.com/abhisheksinha-989/students-api/internal/types"
 
 type Storage interface {
 	CreateStudent(name string, email string, age int) (int64, error)
+	GetStudentById(id int64) (types.Student,error)
 }
