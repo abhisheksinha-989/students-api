c5cb8e55ff60428f6f5a4f42469eb02b827cfa3d
abhisheksinha-989 <amansinha.989@gmail.com>
2026-09-24 16:51:41 +0530

    project skeleton and .gitignore and gomod created

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,9 +1,1 @@
 package storage
-
-import "github.com/abhisheksinha-989/students-api/internal/types"
-
-type Storage interface {
-	CreateStudent(name string, email string, age int) (int64, error)
-	GetStudentById(id int64) (types.Student, error)
-	GetStudents() ([]types.Student, error)
-}
